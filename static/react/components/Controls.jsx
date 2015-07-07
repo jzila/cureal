230079a0b846be72f747c7b0badf502f752180a9
John Zila <john@jzila.com>
2015-07-06 17:27:18 -0700

    Separated data from form. Next step: separate it further so only data is part of state

--- static/react/components/Controls.jsx
+++ static/react/components/Controls.jsx
@@ -47,15 +47,6 @@ var DuplicateFormControl = React.createClass({
     }
 });
 
-var controlIdCounts = {};
-
-var getControlId = function(ct) {
-    if (!controlIdCounts[ct]) {
-        controlIdCounts[ct] = 0;
-    }
-    return ct + "-" + controlIdCounts[ct]++;
-};
-
 var ControlRow = React.createClass({
     render: function() {
         var _this = this;
@@ -66,13 +57,13 @@ var ControlRow = React.createClass({
             if (width >= 12) {
                 widthClass += "$";
             }
-            var id = getControlId(control.type);
+            var id = control.id + "-" + _this.props.formId;
             var text = control.text.replace(/{{form-id}}/, function(match) {
                 return _this.props.formId;
             });
             var value = "";
-            if (_this.props.populate) {
-                value = control.value;
+            if (control.id && _this.props.data && _this.props.data[control.id]) {
+                value = _this.props.data[control.id];
             }
             switch (control.type) {
                 case "name":
@@ -102,52 +93,57 @@ var ControlRow = React.createClass({
 var Form = React.createClass({
     getInitialState: function() {
         var rows = [];
-        var state = {
-            "form-id": 1
-        };
+        var form_id = 1;
+        var state = {};
         if (this.props.form) {
-            key = 0;
-            if (this.props.form.controls && this.props.form.controls.length) {
-                rows = this.props.form.controls.map(function(row) {
+            var makeCreateRow = function(data, key) {
+                return function(row) {
                     return {
                         "row": jQuery.extend(true, [], row),
                         "key": key++,
-                        "form-id": state["form-id"],
-                        "populate": true
+                        "form-id": form_id,
+                        "data": data
                     };
-                });
+                };
+            };
+            if (this.props.form.controls && this.props.form.controls.length) {
+                if (this.props.form.data) {
+                    for (var i=0; i<this.props.form.data.length; i++) {
+                        var data = this.props.form.data[i];
+                        Array.prototype.push.apply(rows, this.props.form.controls.map(makeCreateRow(data, rows.length)));
+                        form_id++;
+                    }
+                } else {
+                    rows = this.props.form.controls.map(makeCreateRow(null, rows.length));
+                    form_id++;
+                }
             }
             state.actionsIndex = rows.length;
             if (this.props.form.actions && this.props.form.actions.length) {
-                Array.prototype.push.apply(rows, this.props.form.actions.map(function(row) {
-                    return {
-                        "row": jQuery.extend(true, [], row),
-                        "key": key++
-                    };
-                }));
+                Array.prototype.push.apply(rows, this.props.form.actions.map(makeCreateRow(null, rows.length)));
             }
-            state.rows = rows;
         }
+        state.rows = rows;
+        state["form-id"] = form_id;
         return state;
     },
     handleDuplicateFormClick: function() {
         if (this.props.form.controls && this.props.form.controls.length) {
             var rows = this.state.rows;
-            var form_id = this.state["form-id"] + 1;
+            var form_id = this.state["form-id"];
             var key = rows.length;
             newRows = this.props.form.controls.map(function(row) {
                 return {
                     "row": jQuery.extend(true, [], row),
                     "key": key++,
-                    "form-id": form_id,
-                    "populate": false
+                    "form-id": form_id
                 };
             });
             Array.prototype.splice.apply(rows, [this.state.actionsIndex, 0].concat(newRows));
             this.setState({
                 rows: rows,
                 actionsIndex: this.state.actionsIndex + newRows.length,
-                "form-id": form_id
+                "form-id": form_id + 1
             });
         }
     },
@@ -157,7 +153,7 @@ var Form = React.createClass({
         }
         var _this = this;
         var rows = this.state.rows.map(function(obj) {
-            return <ControlRow row={obj.row} handleClick={_this.handleDuplicateFormClick} key={obj.key} formId={obj["form-id"]} populate={obj.populate} />;
+            return <ControlRow row={obj.row} handleClick={_this.handleDuplicateFormClick} key={obj.key} formId={obj["form-id"]} data={obj.data} />;
         });
         return (
             <div className="12u$">
